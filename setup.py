ffa805c930a160cf967e36eb90c7374d128b9a88
Stephan Marais <stephan.marais@Entelects-MacBook-Pro.local>
2019-07-27 10:32:35 +0200

    create basic setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='uppercut',
+    version='0.0.1',
+    description='A library to handle common tasks for creating an Uppercut module in python',
+    py_modules=['test'],
+    package_dir={'':'src'}
+)
