3a28da2c7af8fd1a1b9ae3452f45d79d1e4dd02f
Stephan Marais <stephan.marais@Entelects-MacBook-Pro.local>
2019-07-29 11:14:42 +0200

    creat empty uppercut lib

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ setup(
     url='https://github.com/StephanAm/uppercut.python.tools',
     author='Stephan Marais',
     maintainer='Stephan Marais',
-    version='0.0.5',
+    version='0.0.8',
     description='A library to handle common tasks for creating an Uppercut module in python.',
     py_modules=['test'],
     package_dir={'':'src'},
-    packages=find_packages(),
+    packages=find_packages('src'),
     classifiers=[
         "Programming Language :: Python :: 3.7"
     ],
--- src/__init__.py
+++
@@ -1,1 +0,0 @@
-from hwtest import sayHi
